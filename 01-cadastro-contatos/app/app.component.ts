5414bec177f453d988b3aea2c9117f902d8003c5
Erik Brunno <erik.brunno@yahoo.com.br>
2018-07-11 21:33:31 -0300

    primeiro hellow world com angular 2

---
+++ 01-cadastro-contatos/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Minha primeira aplicacao em angular 2</h1>'
+})
+export class AppComponent {
+
+}
